7b61c0551ed7b74e1590cd93d06d4ca70060438e
Łukasz Dziedzic <dziedzic@alfabety.pl>
2022-11-04 23:14:29 +0100

    Drawing path improved i think

--- Package.swift
+++ Package.swift
@@ -9,9 +9,9 @@ let package = Package(
     products: [
         .library(name: "UFOKit", targets: ["UFOKit"]),
     ],
-    dependencies: [
-      .package(url: "https://github.com/dcsch/fontpens.git", from: "0.1.0")
-    ],
+//    dependencies: [
+//      .package(url: "https://github.com/dcsch/fontpens.git", from: "0.1.0")
+//    ],
     targets: [
         .target(name: "UFOKit", dependencies: ["FontPens"]),
         .testTarget(name: "UFOKitTests", dependencies: ["UFOKit"]),
