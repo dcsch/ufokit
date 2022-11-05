bc9235fbadf48aee6464605d2b9ff7ca37736790
Łukasz Dziedzic <dziedzic@alfabety.pl>
2022-11-05 13:26:20 +0100

    Jakoś działa i z cacheami

--- Package.swift
+++ Package.swift
@@ -12,6 +12,10 @@ let package = Package(
 //    dependencies: [
 //      .package(url: "https://github.com/dcsch/fontpens.git", from: "0.1.0")
 //    ],
+    dependencies: [
+            .package(url: "/Users/lukasz/Documents/XCode/FontPens", from: "1.0.0")
+            //.package(path: "../Clibgit") // is also fine
+        ],
     targets: [
         .target(name: "UFOKit", dependencies: ["FontPens"]),
         .testTarget(name: "UFOKitTests", dependencies: ["UFOKit"]),
